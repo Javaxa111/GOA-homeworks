7fef472c43176309c45d344bafafb395074cd9fc
Javaxa111 <javakhishviligiorg5@gmail.com>
2025-10-05 17:04:51 +0400

    second commit

---
+++ Variables.py
@@ -0,0 +1,13 @@
+#შექმენით 4 ცვლადი პირველში შეინახეთ თქვენი სახელი, მეორეში შეინახეთ თქვენი გვარი მესამეში თქვენი ასაკი და მეოთხეში თქვენი სიმაღლე, მოცემული ცვლადები დაბეჭდეთ
+
+my_name=("giorgi")
+print(my_name)
+
+my_last_name=("javakhishvili")
+print(my_last_name)
+
+my_age=(15)
+print(my_age)
+
+my_height=(177)
+print(my_height)
