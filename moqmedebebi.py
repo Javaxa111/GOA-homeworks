4a273206d0b2385d7c58f97995ea8e98c4a04473
Javaxa111 <javakhishviligiorg5@gmail.com>
2025-10-12 01:58:48 +0400

    second commit

---
+++ moqmedebebi.py
@@ -0,0 +1,25 @@
+
+#შექმენით ორი ცვლადი მათში შეინახეთ რაიმე რიცხვები თქვენი დავალებაა რომ ამ ორ რიცხვზე მოახდინოთ მათემატიკური ოპერაციები როგორიცაა +-*/
+
+number1 = 12
+number2 = 33
+
+jami = number1 + number2
+print(jami)
+
+sxvaoba = number1 - number2
+print(sxvaoba)
+
+namravli = number1 * number2
+print(namravli)
+
+gayofa = number1 / number2
+print(gayofa)
+
+
+
+
+
+
+age = 25
+print(age + 5)
